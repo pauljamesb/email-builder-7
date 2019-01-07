6bb6bc98778e7881c7bdf1f773e52f46e6866240
pauljamesb <37079324+pauljamesb@users.noreply.github.com>
2019-01-07 14:15:56 +0000

    Update EmailBuilderController.cs

--- Controllers/EmailBuilderController.cs
+++ Controllers/EmailBuilderController.cs
@@ -1,1 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DNCT5.Data;
+using Microsoft.AspNetCore.Mvc;
 
+namespace Eb7.Controllers
+{
+        public class EB7 : Controller
+        {
+        }
+}
